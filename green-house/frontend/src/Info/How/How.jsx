a829c5d696f0fd9103b881a7dfbd570e13be5dcf
Nuno Moniz <nuno-moniz_student2022@wilder.school>
2022-05-12 16:43:43 +0100

    Created Info branch and created the info components

---
+++ green-house/frontend/src/Info/How/How.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function How() {
+    return (
+        <div>
+            <h1>How ?</h1>
+            <div className="stepOne">
+                <p>
+                Insert your trip Begin-Point and End-Point
+                </p>
+            </div>
+            <div className="stepTwo">
+                <p>
+                We calculate based on the type of vehicle the amount of CO2 emitted 
+                </p>
+            </div>
+            <div className="stepThree">
+                <p>
+                You earn Points based on the amount of CO2 you dont pollute 
+                </p>
+            </div>
+        </div>
+    )
+}
