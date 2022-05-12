57eba323921c29e6366f164956b86725a46fc4a6
Nuno Moniz <nuno-moniz_student2022@wilder.school>
2022-05-12 22:44:56 +0100

    added graph to Why and edited css

--- green-house/frontend/src/Components/Content/Why.jsx
+++ green-house/frontend/src/Components/Content/Why.jsx
@@ -5,14 +5,22 @@ export default function Why() {
     return (
         <div className="whyContainer">
             <div className="why-title">
-            <h1>Why ?</h1>
+                <h1>Why ?</h1>
             </div>
+            <div className="why-content">
                 <div className="whyText">
                     <p>
                     To reduce the amount of CO2 produced by transportation on a day-to-day basis for greener cities
-and so that people are more aware of how they can be part of the change and have new habits.
+                    and so that people are more aware of how they can be part of the change and have new habits.
                     </p>
                 </div>
+                <div className="graph-container">
+                <img src="https://www.statista.com/graphic/1/1185535/transport-carbon-dioxide-emissions-breakdown.jpg" 
+                    alt="Statistic: Distribution of carbon dioxide emissions produced by the transportation sector worldwide in 2020, by subsector | Statista" 
+                    />
+                </div>
+            </div>
+            
         </div>
     )
 }
