18c5da3d9100828cde5d9be254117d5daf8f70d5
Juan Giménez <juanp.gi2485@gmail.com>
2022-05-12 23:11:47 +0100

    create map

--- green-house/frontend/src/components/home/Home.jsx
+++ green-house/frontend/src/components/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Map from '../map/Map';
 
 function Home() {
     return (
         <div>
             <h1>Home</h1>
+            <Map />
         </div>
     );
 }
