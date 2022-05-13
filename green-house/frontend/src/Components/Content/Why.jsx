661ec5cd5cf6fff8990c858564d61b8051c5c41f
Nuno Moniz <nuno-moniz_student2022@wilder.school>
2022-05-13 11:41:38 +0100

    Updated text of why and added title

--- green-house/frontend/src/Components/Content/Why.jsx
+++ green-house/frontend/src/Components/Content/Why.jsx
@@ -10,10 +10,10 @@ export default function Why() {
             <div className="why-content">
                 <div className="whyText">
                     <h2>
-                        Distribution of carbon dioxide emissions produced by the transportation sector worldwide in 2020, by subsector.
+                    Distribution of carbon dioxide emissions produced by the transportation sector worldwide in 2020, by subsector.
                     </h2>
                     <p>
-                    To reduce the amount of CO2 produced by transportation on a day-to-day basis for more ecologic cities and with the goal of raising awareness to people so they are more aware of how they can be part of the change and have new  and healthier habits.
+                    To reduce the amount of CO2 produced by transportation on a day-to-day basis for more ecologic cities and with the goal of raising awareness to people.
                     </p>
                 </div>
                 <div className="graph-container">
